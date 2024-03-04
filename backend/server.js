cf328b29458c7037488974ee49116cffead37d15
choeurtis <58913206+choeurtis18@users.noreply.github.com>
2024-03-04 20:58:02 +0100

    Optimisation du code

--- backend/server.js
+++ backend/server.js
@@ -3,70 +3,58 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
-// Multiple routing
-const router1 = express.Router();
-const router2 = express.Router();
-const router3 = express.Router();
 
-router1.get('/posts', async function (req, res, next) {
-    console.log("get posts");
+const router = express.Router();
 
-    const options = {
-        method: 'GET',
-        url: 'https://jsonplaceholder.typicode.com/posts'
-    };
 
-    try {
-        const response = await axios.request(options);
-        console.log(response.data);
-        res.send(response.data);
-    } catch (error) {
-        console.error(error);
-        res.status(500).send("An error occurred"); 
-    }
+app.use((err, req, res, next) => {
+    console.error(err);
+    res.status(500).send("An error occurred");
 });
 
-router2.get('/posts/:id', async function (req, res, next) {
-    console.log("get post by id");
+const asyncHandler = fn => (req, res, next) => Promise.resolve(fn(req, res, next)).catch(next);
 
-    const postId = req.params.id;
-    const options = {
-        method: 'GET',
-        url: `https://jsonplaceholder.typicode.com/posts/${postId}`
-    };
+// Get
+router.get('/posts', asyncHandler(async (req, res) => {
+    const response = await axios.get('https://jsonplaceholder.typicode.com/posts');
+    res.send(response.data);
+}));
+router.get('/posts/:id', asyncHandler(async (req, res) => {
+    const response = await axios.get(`https://jsonplaceholder.typicode.com/posts/${req.params.id}`);
+    res.send(response.data);
+}));
+router.get('/posts/:id/comments', asyncHandler(async (req, res) => {
+    const response = await axios.get(`https://jsonplaceholder.typicode.com/posts/${req.params.id}/comments`);
+    res.send(response.data);
+}));
 
-    try {
-        const response = await axios.request(options);
-        console.log(response.data);
-        res.send(response.data);
-    } catch (error) {
-        console.error(error);
-        res.status(500).send("An error occurred");
-    }
+// Post
+router.post('/posts', asyncHandler(async (req, res) => {
+    const response = await axios.post('https://jsonplaceholder.typicode.com/posts', req.body);
+    res.status(201).send(response.data);
+}));
+router.post('/posts/:id/comments', asyncHandler(async (req, res) => {
+    const response = await axios.post(`https://jsonplaceholder.typicode.com/posts/${req.params.id}/comments`);
+    res.send(response.data);
+}));
+
+//Delete
+router.delete('/posts/:id', asyncHandler(async (req, res) => {
+    await axios.delete(`https://jsonplaceholder.typicode.com/posts/${req.params.id}`);
+    res.status(204).send();
+}));
+
+//Update
+router.put('/posts/:id', asyncHandler(async (req, res) => {
+    const response = await axios.put(`https://jsonplaceholder.typicode.com/posts/${req.params.id}`, req.body);
+    res.send(response.data);
+}));
+
+app.use('/', router);
+
+app.listen(PORT, () => {
+    console.log(`Server listening on PORT ${PORT}`);
 });
-
-router3.get('/posts/:id/comments', async function (req, res, next) {
-    console.log("get post comments by id");
-
-    const postId = req.params.id;
-    const options = {
-        method: 'GET',
-        url: `https://jsonplaceholder.typicode.com/posts/${postId}/comments`
-    };
-
-    try {
-        const response = await axios.request(options);
-        console.log(response.data);
-        res.send(response.data);
-    } catch (error) {
-        console.error(error);
-        res.status(500).send("An error occurred");
-    }
-});
-
-app.use(router1);
-app.use(router2);
-app.use(router3);
 
 
 app.listen(PORT, function (err) {
