81f57ddee433b3e6b787393ab209451f63320400
Mouhamadou-Soumare <82391111+Mouhamadou-Soumare@users.noreply.github.com>
2024-03-21 21:38:13 +0100

    first deploy process

--- backend/index.js
+++ backend/index.js
@@ -20,6 +20,14 @@ app.use('/footballapi', liveMatchesRouter);
 app.use('/auth', authRouter);
 app.use('/news', newsRouter.default); 
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'https://mangi-client.vercel.app');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
+
+
 app.use((err, req, res, next) => {
     console.error(err);
     res.status(500).send("An error occurred");
